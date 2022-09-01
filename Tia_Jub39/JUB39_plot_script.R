22b6d08a5a5cd539e782db9b37c14cbd6e6f3311
tibr36 <browntia365@gmail.ecom>
2022-09-01 16:03:29 -0400

    Jub39 100pct

---
+++ Tia_Jub39/JUB39_plot_script.R
@@ -0,0 +1,22 @@
+Date: "09/31/2022"
+100% octanol with N2 and cest-2.1 Providencia SOS
+---
+
+library(tidyverse)
+library(ggplot2)
+theme_set(theme_classic())
+####### select a file in the data folder ####
+files <- fs::dir_ls(recurse = TRUE, glob = "Tia_Jub39/2022-08-31_cest-2.1*.csv")
+data <- files %>% purrr::map_df(., readr::read_csv, .id = "filename")
+
+####### anova ####
+lm(data = data, formula = Response_Latency ~ Genotype*Condition) %>%
+  summary()
+
+###### plot #####
+data %>% ggplot(aes(x = Condition, y = Response_Latency)) +
+  stat_summary(geom = "bar", aes(fill = Genotype, alpha = Condition), width = 0.75) +
+  stat_summary(geom = "errorbar", fun.data = "mean_se", width = .2) +
+  coord_cartesian( ylim=c(0,10), expand = FALSE) +  
+  facet_grid(~Genotype) +
+  scale_fill_manual(values = c("blue", "green"))
