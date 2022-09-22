062fc7ddc8229d4df90d8726f1fae5218279fbb0
tibr36 <browntia365@gmail.ecom>
2022-09-22 14:13:08 -0400

    aggregate SOS with MD data

--- Tia_Jub39/JUB39_plot_script.R
+++ Tia_Jub39/JUB39_plot_script.R
@@ -1,4 +1,4 @@
-Date: "0906/2022"
+Date: "09/21/2022"
 100% octanol with N2 and cest-2.1 Providencia SOS
 ---
 
@@ -21,13 +21,21 @@ data %>% ggplot(aes(x = Condition, y = Response_Latency)) +
   facet_grid(~Genotype) +
   scale_fill_manual(values = c("blue", "green"))
 
-###### plot (excluding "8-31"") #####
-data %>% 
-  filter(Date != "2021-08-31") %>%
-  ggplot(aes(x = Condition, y = Response_Latency)) +
+###### plot (with space between x axis and N2/cest-2.1) #####
+  data %>% ggplot(aes(x = Condition, y = Response_Latency)) +
   stat_summary(geom = "bar", aes(fill = Genotype, alpha = Condition), width = 0.75) +
   stat_summary(geom = "errorbar", fun.data = "mean_se", width = .2) +
   labs(title = "100% octanol avoidance with N2 and cest-2.1 on OP50 vs Providencia") +
+  facet_grid(~Genotype) +
+  scale_fill_manual(values = c("blue","green")) +
+  scale_color_manual(values = c("blue","green"))
+
+###### plot (Most recent experiment day) #####
+  subset(data, Date== "2022-09-21")%>%
+  ggplot(aes(x = Condition, y = Response_Latency)) +
+  stat_summary(geom = "bar", aes(fill = Genotype, alpha = Condition), width = 0.75) +
+  stat_summary(geom = "errorbar", fun.data = "mean_se", width = .2) +
+  labs(title = "100% SOS with N2 and cest-2.1 on OP50 vs Providencia(2022-09-21)") +
   facet_grid(~Genotype) +
   scale_fill_manual(values = c("blue","green")) +
   scale_color_manual(values = c("blue","green"))
